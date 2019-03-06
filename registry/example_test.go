1de81d29c6911460e2c0b1bcd1fd337747f4e4c2
Nicholas Wiersma <nick@wiersma.co.za>
2019-03-06 16:34:46 +0100

    Show create in example

--- registry/example_test.go
+++ registry/example_test.go
@@ -17,6 +17,15 @@ func Example() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
+	fmt.Println("schema: ", schema)
+
+	schemaStr := `["null","string","int"]`
+	id, schema, err := reg.IsRegistered("foobar", schemaStr)
+	if err != nil {
+		id, schema, err = reg.CreateSchema("foobar", schemaStr)
+	}
+
+	fmt.Println("id: ", id)
 	fmt.Println("schema: ", schema)
 }
