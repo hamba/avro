c74aaf22aede0252a993b6d635f139b4af255d1e
MDP <33422288+mdpaquin@users.noreply.github.com>
2022-11-29 14:35:22 -0500

    feat: add config option to disable encoder/decoder caching (#217)

--- config_internal_test.go
+++ config_internal_test.go
@@ -44,6 +44,33 @@ func TestConfig_ReusesDecoders(t *testing.T) {
 	assert.Same(t, dec1, dec2)
 }
 
+func TestConfig_DisableCache_DoesNotReuseDecoders(t *testing.T) {
+	type testObj struct {
+		A int64 `avro:"a"`
+	}
+
+	api := Config{
+		TagKey:         "test",
+		BlockLength:    2,
+		DisableCaching: true,
+	}.Freeze()
+	cfg := api.(*frozenConfig)
+
+	schema := MustParse(`{
+	"type": "record",
+	"name": "test",
+	"fields" : [
+		{"name": "a", "type": "long"}
+	]
+}`)
+	typ := reflect2.TypeOfPtr(&testObj{})
+
+	dec1 := cfg.DecoderOf(schema, typ)
+	dec2 := cfg.DecoderOf(schema, typ)
+
+	assert.NotSame(t, dec1, dec2)
+}
+
 func TestConfig_ReusesEncoders(t *testing.T) {
 	type testObj struct {
 		A int64 `avro:"a"`
@@ -68,4 +95,31 @@ func TestConfig_ReusesEncoders(t *testing.T) {
 	enc2 := cfg.EncoderOf(schema, typ)
 
 	assert.Same(t, enc1, enc2)
+}
+
+func TestConfig_DisableCache_DoesNotReuseEncoders(t *testing.T) {
+	type testObj struct {
+		A int64 `avro:"a"`
+	}
+
+	api := Config{
+		TagKey:         "test",
+		BlockLength:    2,
+		DisableCaching: true,
+	}.Freeze()
+	cfg := api.(*frozenConfig)
+
+	schema := MustParse(`{
+	"type": "record",
+	"name": "test",
+	"fields" : [
+		{"name": "a", "type": "long"}
+	]
+}`)
+	typ := reflect2.TypeOfPtr(testObj{})
+
+	enc1 := cfg.EncoderOf(schema, typ)
+	enc2 := cfg.EncoderOf(schema, typ)
+
+	assert.NotSame(t, enc1, enc2)
 }
