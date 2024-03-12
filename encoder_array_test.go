9d79378823012a54f437de514cae3bfe5f76318a
settshalex <arctgalex@gmail.com>
2024-03-12 07:44:43 +0100

    support for recursive struct

--- encoder_array_test.go
+++ encoder_array_test.go
@@ -51,9 +51,18 @@ func TestEncoder_ArrayRecursiveStruct(t *testing.T) {
 	buf := bytes.NewBuffer([]byte{})
 	enc, err := avro.NewEncoder(schema, buf)
 	log.Print(buf.Bytes())
-	err = enc.Encode([]TestRecord{{A: 12, B: "aaa", C: &TestRecord{A: 13, B: "bbb", C: &TestRecord{A: 44, B: "ccc", C: nil}}}, {A: 27, B: "ddd", C: &TestRecord{A: 13, B: "eee", C: nil}}})
+	slice := []TestRecord{{A: 12, B: "aaa", C: &TestRecord{A: 13, B: "bbb", C: &TestRecord{A: 44, B: "ccc", C: nil}}}, {A: 27, B: "ddd", C: &TestRecord{A: 13, B: "eee", C: nil}}}
+	err = enc.Encode(slice)
 	require.NoError(t, err)
 	assert.Equal(t, []byte{0x3, 0x3c, 0x18, 0x6, 0x61, 0x61, 0x61, 0x0, 0x1a, 0x6, 0x62, 0x62, 0x62, 0x0, 0x58, 0x6, 0x63, 0x63, 0x63, 0x2, 0x36, 0x6, 0x64, 0x64, 0x64, 0x0, 0x1a, 0x6, 0x65, 0x65, 0x65, 0x2, 0x0}, buf.Bytes())
+
+	dec, err := avro.NewDecoder(schema, bytes.NewReader(buf.Bytes()))
+	require.NoError(t, err)
+
+	var str []TestRecord
+	err = dec.Decode(&str)
+	assert.Equal(t, slice, str)
+	assert.Error(t, err)
 }
 
 func TestEncoder_Array(t *testing.T) {
