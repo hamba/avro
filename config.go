0825c2a14b47efa0a74c4d48de2c668836c026a8
Brian Shih <brianshihstart@gmail.com>
2024-04-20 12:39:58 -0400

    Remove defaultMax variable

--- config.go
+++ config.go
@@ -12,8 +12,7 @@ const (
 	defaultMaxByteSliceSize = 1_048_576 // 1 MiB
 	// Max allocation size for an array due to the limit in number of bits in a heap address:
 	// https://github.com/golang/go/blob/7f76c00fc5678fa782708ba8fece63750cb89d03/src/runtime/malloc.go#L183
-	maxAllocSize             = int(1 << 48)
-	defaultMaxSliceAllocSize = maxAllocSize
+	maxAllocSize = int(1 << 48)
 )
 
 // DefaultConfig is the default API.
