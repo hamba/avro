1ad8b7129a095b5e57abb32166e32c33593cba2c
Kim Gräsman <kim.grasman@gmail.com>
2025-04-01 17:51:29 +0200

    Bundle generic Encoder/Decoder into GenericCodec

--- soe/generic.go
+++ soe/generic.go
@@ -4,54 +4,42 @@ import (
 	"github.com/hamba/avro/v2"
 )
 
-// GenericEncoder encodes AvroGenerated values into SOE-framed Avro binary
-// payloads. Must be instantiated with a pointer type, e.g.
-//    enc, _ := NewGenericEncoder[*MyType]()
+// GenericCodec marshals/unmarshals AvroGenerated values to/from SOE-framed Avro
+// binary payloads. Must be instantiated with a pointer type, e.g.
+//
+//    c, _ := NewGenericCodec[*MyType]()
 //    val MyType
-//    enc.Encode(val)
-type GenericEncoder[T any] struct {
+//    c.Encode(val)
+//    c.Decode(&val)
+//
+// It is a strongly typed version of soe.Codec.
+type GenericCodec[T any] struct {
 	codec *Codec
 }
 
-func NewGenericEncoder[T AvroGenerated](api avro.API) (*GenericEncoder[T], error) {
+func NewGenericCodec[T AvroGenerated](api avro.API) (*GenericCodec[T], error) {
 	schema := GetSchema[T]()
 	coder, err := NewCodecWithAPI(schema, api)
 	if err != nil {
 		return nil, err
 	}
-	return &GenericEncoder[T]{
+	return &GenericCodec[T]{
 		codec: coder,
 	}, nil
 }
 
-func (e *GenericEncoder[T]) Encode(v T) ([]byte, error) {
-	return e.codec.Encode(v)
+// Encode marshals a typed value to SOE-encoded Avro binary.
+func (c *GenericCodec[T]) Encode(v T) ([]byte, error) {
+	return c.codec.Encode(v)
 }
 
-// GenericDecoder decodes SOE-framed records into values whose type implement
-// AvroGenerated. Must be instantiated with a pointer type, e.g.
-//    dec, _ := NewGenericDecoder[*MyType]()
-//    val MyType
-//    dec.Decode(data, &val)
-type GenericDecoder[T any] struct {
-	codec *Codec
+// Decode unmarshals a typed value from SOE-encoded Avro binary.
+func (c *GenericCodec[T]) Decode(data []byte, v T) error {
+	return c.codec.Decode(data, v)
 }
 
-func NewGenericDecoder[T AvroGenerated](api avro.API) (*GenericDecoder[T], error) {
-	schema := GetSchema[T]()
-	coder, err := NewCodecWithAPI(schema, api)
-	if err != nil {
-		return nil, err
-	}
-	return &GenericDecoder[T]{
-		codec: coder,
-	}, nil
-}
-
-func (d *GenericDecoder[T]) Decode(data []byte, v T) error {
-	return d.codec.Decode(data, v)
-}
-
-func (d *GenericDecoder[T]) DecodeStrict(data []byte, v T) error {
-	return d.codec.DecodeStrict(data, v)
+// DecodeStrict unmarshals a typed value from SOE-encoded Avro binary, and fails
+// if the schema fingerprint doesn't match the held schema.
+func (c *GenericCodec[T]) DecodeStrict(data []byte, v T) error {
+	return c.codec.DecodeStrict(data, v)
 }
