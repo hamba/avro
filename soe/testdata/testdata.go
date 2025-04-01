c5a81c33c21514b090aab67b715d3861830b56fe
Kim Gräsman <kim.grasman@gmail.com>
2025-04-01 18:17:00 +0200

    Add test type that implements AvroGenerated

--- soe/testdata/testdata.go
+++ soe/testdata/testdata.go
@@ -11,3 +11,26 @@ type StringInt struct {
 }
 
 var StringIntSchema = avro.MustParse(`{"name":"stringint","type":"record","fields":[{"name":"stringval","type":"string"},{"name":"intval","type":"int"}]}`)
+
+// Faux avrogen-generated type
+type Generated struct {
+	Name string `avro:"name"`
+	Age  int    `avro:"age"`
+}
+
+var generatedSchema = avro.MustParse(`{"name":"generated","type":"record","fields":[{"name":"name","type":"string"},{"name":"age","type":"int"}]}`)
+
+// Schema returns the schema for Generated.
+func (o *Generated) Schema() avro.Schema {
+	return generatedSchema
+}
+
+// Unmarshal decodes b into the receiver.
+func (o *Generated) Unmarshal(b []byte) error {
+	return avro.Unmarshal(o.Schema(), b, o)
+}
+
+// Marshal encodes the receiver.
+func (o *Generated) Marshal() ([]byte, error) {
+	return avro.Marshal(o.Schema(), o)
+}
